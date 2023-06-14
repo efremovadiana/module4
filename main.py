604e9934e572578f826a447d5611e6db98d705d1
Diana <dianaefremova.07@mail.ru>
2023-06-14 20:26:15 +0600

    init repo

---
+++ main.py
@@ -0,0 +1,10 @@
+def strcounter(s: str):
+    #O(N)
+    syms_counter = {} #словарь
+    for sym in s:
+        syms_counter[sym] = syms_counter.get(sym, 0) + 1
+
+    print(syms_counter)
+strcounter("hellohellohello")
+
+#https://github.com/efremovadiana/module4.git
